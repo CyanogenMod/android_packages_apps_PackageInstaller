66fa0c4795d66c51c167d841d74fb8e7056f3ec5
emancebo <emancebo@cyngn.com>
2015-07-08 13:15:05 -0700

    PackageInstaller: add hook to manage packages via PRE_BOOT_COMPLETED event
    Change-Id: I797fd93eedd3c440bfd906c685bb6b11acb1c9c9

---
+++ pre_boot_listener/src/com/android/packageinstaller/PackageInstallerPreBootListener.java
@@ -0,0 +1,11 @@
+package com.android.packageinstaller;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+// stub receiver
+public class PackageInstallerPreBootListener extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent broadcast) {}
+}
